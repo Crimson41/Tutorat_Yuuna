2ed299f85b9ef9abc07efbe196705778742b6a4a
Emilie Gonin <emilie.yuu@gmail.com>
2024-02-18 17:29:17 +0100

    improve tutorat

--- Tutorat-CPP/Tutorat-CPP-Console/Main.cpp
+++ Tutorat-CPP/Tutorat-CPP-Console/Main.cpp
@@ -2,11 +2,6 @@
 #include "TutoratAdvanced.h"
 #include "TutoratRPG_GameManager.h"
 
-// À faire dans l'ordre :
-// 1 - TutoratBasic
-// 2 - TutoratAdvanced
-// 3 - TutoratRPG
-
 int main()
 {
     TutoratBasic tb;
